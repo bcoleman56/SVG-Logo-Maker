d30917d6481df1d112f9127a1174bd90c6ac33a9
bcoleman56 <brettcoleman369@gmail.com>
2023-06-12 11:41:39 -0400

    fixed prompt validation

--- Develop/utils/validate.js
+++ Develop/utils/validate.js
@@ -3,18 +3,9 @@ const colorList = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'brown'
 
 const regex = /[0-9a-z]+/i;
 
-function validColor(color){
+const validateColor = (color) => {
     if(colorList.includes(color)){
         return true
-    } else if (color.length === 6 && parseInt(color[0])){
-        // could be hex color without #
-        for (let i = 0; i < color.length; i++){
-            if(!color[i].match(regex)){
-                console.log(`This ${color[i]} is not hexidecimal or in the color list`)
-                return false
-            }
-        }
-
     } else if (color.length === 7 && color[0] === '#'){
         // could be hex color with #
         for (let i = 1; i < color.length; i++){
@@ -23,7 +14,9 @@ function validColor(color){
                 return false
             }
         }
+    } else {
+        return false
     }
 }
 
-module.exports = validColor;
+module.exports = validateColor;
